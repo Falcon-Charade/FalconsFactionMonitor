b028ae164a29690a45e6ce9889993c8d0b81581f
Falcon Charade <conradfolscher@yahoo.com.au>
2025-02-28 22:59:32 +1000

    Moved SQL Connection Handler
    SQL connection now only happens in one spot, when trying to send the journal files. This was moved in case the database goes into pause mode while playing (Likely to happen if idle). This would have broken the program
    
    #12

--- FalconsFactionMonitor/FalconsFactionMonitor/Services/JournalRetrieval.cs
+++ FalconsFactionMonitor/FalconsFactionMonitor/Services/JournalRetrieval.cs
@@ -30,30 +30,6 @@ namespace FalconsFactionMonitor.Services
                 string storedProc = File.ReadAllText(filePath);
                 string connectionString = Registry.GetValue(@"HKEY_CURRENT_USER\SOFTWARE\Falcon Charade", "FalconsFactionMonitorDbConnection", null).ToString();
                 SqlConnection connection = new SqlConnection(connectionString);
-                for (int i = 1; i <= 5; i++)
-                {
-                    try
-                    {
-                        connection.Open();
-                        break;
-                    }
-                    catch (Exception ex)
-                    {
-                        if (ex.Message.StartsWith("Database 'FalconsFactionMonitor' on server 'falcons-sql.database.windows.net' is not currently available."))
-                        {
-                            if (i == 5)
-                            {
-                                throw;
-                            }
-                            Thread.Sleep(10000);
-                        }
-                        else
-                        {
-                            throw;
-                        }
-                    }
-                }
-                connection.Close();
 
                 // Subscribe to the OnFSDJumpDetected event
                 monitor.OnFSDJumpDetected += factions =>
@@ -61,7 +37,29 @@ namespace FalconsFactionMonitor.Services
                     //Console.WriteLine("New FSD Jump detected! Processing factions...");
                     using SqlConnection connection = new SqlConnection(connectionString);
                     {
-                        connection.Open();
+                        for (int i = 1; i <= 5; i++)
+                        {
+                            try
+                            {
+                                connection.Open();
+                                break;
+                            }
+                            catch (Exception ex)
+                            {
+                                if (ex.Message.StartsWith("Database 'FalconsFactionMonitor' on server 'falcons-sql.database.windows.net' is not currently available."))
+                                {
+                                    if (i == 5)
+                                    {
+                                        throw;
+                                    }
+                                    Thread.Sleep(10000);
+                                }
+                                else
+                                {
+                                    throw;
+                                }
+                            }
+                        }
                         foreach (var faction in factions.OrderByDescending(f => f.InfluencePercent))
                         {
                             Console.ForegroundColor = ConsoleColor.Yellow;
